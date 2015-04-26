3964ad3069c9ff3d583370d16b295329bb80c3ac
profiod <profiod@onid.oregonstate.edu>
2015-04-26 19:37:58 -0400

    completed styling, adding and removing favorites, and allowing for pagination selection

--- gist_processor.js
+++ gist_processor.js
@@ -5,54 +5,103 @@ function Gist(gistID, gistDesc, gistURL){
   this.fav = false; 
 }
 
-var favGists = []; 
-var normGists = []; 
-var allGists = []; 
+var favGists = new Array(); 
+var normGists = new Array(); 
+
+function findById(_gistID, listName) {
+  if (listName == "fav") { 
+    for (var i = 0; i < favGists.length; i++) {
+      if (favGists[i].gistID === _gistID) { 
+        return i; 
+      }
+    }
+  }
+  else {
+    for (var i = 0; i < normGists.length; i++) {
+      if (normGists[i].gistID === _gistID) {
+        return i; 
+      }
+    }
+  } 
+}
+
+function favoriteGistAdd(i) { 
+  favGists.push(normGists[i]);     
+  createGistList("fav"); 
+  normGists.splice(i, 1);  
+  createGistList("norm"); 
+  localStorage.setItem("favGists", JSON.stringify(favGists)); 
+}
+
+function favoriteGistRem(i) {
+  favGists.splice(i, 1);  
+  
+  createGistList("fav"); 
+  localStorage.setItem("favGists", JSON.stringify(favGists)); 
+}
 
 function createGistList(listName) {
-  var ul; 
+  var div; 
   var gistList;
-  if (listName === "fav") { 
-    ul = document.getElementById("fav-list"); 
+  if (listName == "fav") { 
+    div = document.getElementById("fav-list"); 
     gistList = favGists; 
   }
   else {
-    ul = document.getElementById("norm-list"); 
+    div = document.getElementById("norm-list"); 
     gistList = normGists; 
   }
-  for (var k = ul.childNodes.length - 1; k >= 0; k--) {
-    ul.removeChild(ul.childNodes[k]);
+  for (var k = div.childNodes.length - 1; k >= 0; k--) {
+    div.removeChild(div.childNodes[k]);
   }
-  gistList.forEach(function(_gist) {
-    var li = document.createElement("li");
-    li.appendChild(listGists(_gist));
-    ul.appendChild(li);
+  gistList.forEach(function (_gist) {
+    var innerDiv = document.createElement("div");
+    innerDiv.id = _gist.gistID; 
+    // button code adapted from professor Soroush Ghorashi Piazza post
+    if (listName == "fav") {
+      var fbutton = document.createElement("button"); 
+      fbutton.innerHTML = "-"; 
+      fbutton.setAttribute("gistID", _gist.gistID); 
+      fbutton.onclick = function() {
+        var gistID = this.getAttribute("gistID"); 
+        var toBeRemovedGist = findById(gistID, listName);
+        favoriteGistRem(toBeRemovedGist);  
+      }
+    }
+    else {
+      var fbutton = document.createElement("button"); 
+      fbutton.innerHTML = "+"; 
+      fbutton.setAttribute("gistID", _gist.gistID); 
+      fbutton.onclick = function() {
+        var gistID = this.getAttribute("gistID"); 
+        var toBeFavoritedGist = findById(gistID, listName);
+        favoriteGistAdd(toBeFavoritedGist);  
+      }
+    }
+    innerDiv.appendChild(fbutton); 
+    var descDiv = document.createElement("span");
+    descDiv.innerHTML = _gist.gistDesc; 
+    innerDiv.appendChild(descDiv);
+    var br = document.createElement("br"); 
+    innerDiv.appendChild(br); 
+    var link = document.createElement("a"); 
+    var text = document.createTextNode(_gist.gistURL.toString()); 
+    link.appendChild(text); 
+    link.title = "Gist URL"; 
+    link.href = _gist.gistURL; 
+    innerDiv.appendChild(link); 
+    div.appendChild(innerDiv);
   });
 }
 
-function listGists(_gist) { 
-  var dl = document.createElement("dl"); 
-  var info = dlGists("Description", _gist.gistDesc); 
-  dl.appendChild(info.dt);
-  dl.appendChild(info.dd); 
-  var info = dlGists("URL", _gist.gistURL); 
-  dl.appendChild(info.dt); 
-  dl.appendChild(info.dd); 
-  return dl;  
-}
-
-function dlGists (name, desc) {
-  var dt = document.createElement("dt"); 
-  var dd = document.createElement("dd"); 
-  dt.innerHTML = name; 
-  dd.innerHTML = desc; 
-  return {"dt":dt, "dd":dd}; 
-}
 
 function gist_request() {
 	var url = "https://api.github.com/gists/public?page=1"; 
-  
-  for (var i = 1 ; i <= 1; i++){	
+  normGists = [];
+  var pageSelect = document.getElementById("page-numbers"); 
+  var pageNum = pageSelect.options[pageSelect.selectedIndex].value;
+
+  for (var i = 1; i <= pageNum; i++){	
     var req = new XMLHttpRequest(); 
   	if(!req){
   		throw "Unable to create the Http Request"; 
@@ -71,12 +120,14 @@ function gist_request() {
           var _gistID = _gistList[j].id; 
           var _gistDesc = _gistList[j].description; 
           var inFavs = false; 
-          if (_gistDesc === "")
+          if (_gistDesc === "" || _gistDesc === null)
             _gistDesc = "No Description Provided"; 
-          for (var k = 0; k < favGists.length; k++) {
-            if (_gistID == favGists[k].gistID) {
-              inFavs = true; 
-              break; 
+          if (favGists !== null) {
+            for (var k = 0; k < favGists.length; k++) {
+             if (_gistID == favGists[k].gistID) {
+                inFavs = true; 
+               break; 
+              }
             }
           }
           if (!inFavs) {
@@ -84,7 +135,7 @@ function gist_request() {
             normGists.push(newGist);
           } 
         }
-        createGistList(); 
+        createGistList("norm"); 
       }
     }
   }
@@ -94,9 +145,9 @@ window.onload = function () {
   favGists = localStorage.getItem("favGists"); 
   if (favGists !== null) {
     favGists = JSON.parse(favGists); 
+    createGistList("fav"); 
   }
-  console.log(favGists); 
-  gist_request(); 
-  createGistList(); 
-  console.log(normGists); 
+  else {
+    favGists = new Array();
+  }
 }   
